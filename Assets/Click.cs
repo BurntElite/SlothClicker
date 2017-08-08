e451df9bdcb4bd54908e9cfdf7bc534ba2119cbc
Taylor Zieman <Tzieman6451@gmail.com>
2017-08-07 20:38:41 -0500

    First Initialization Commit

---
+++ Assets/Click.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Click : MonoBehaviour {
+    public float clicked;
+    private float clickPS;
+    private float clickA;
+
+    private bool checkClick;
+
+	void Start () {
+        clickA = 1;
+	}
+	
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            checkClick = true;
+        }
+	}
+
+    private void FixedUpdate()
+    {
+        if(checkClick == true)
+        {
+            ClickScore();
+        }
+    }
+
+    private void ClickScore(){
+        clicked = clicked + (clickA);
+        checkClick = false;
+	}
+}
---
+++ Assets/ScoreGui.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreGui : MonoBehaviour {
+
+    public Text scoreDisplay;
+    Click clickScript;
+    private float score;
+	// Use this for initialization
+	void Start () {
+        clickScript = GameObject.Find("LevelManager").GetComponent<Click>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        score = clickScript.clicked;
+        ScoreAdd();
+	}
+
+    private void ScoreAdd()
+    {
+        scoreDisplay.text = "Clicks: " + score;
+    }
+}
